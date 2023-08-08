5d217db490bf4498eb5ade9a9afdc4d75e185739
Sheila Macharia <111003634+sheilamacha@users.noreply.github.com>
2023-08-08 14:50:59 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,18 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
-
-    # Construct the API URL for the subreddit's information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    try:
-        # Send a GET request to the API
-        response = requests.get(url, headers=headers)
-
-        # Check if the response is successful
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        elif response.status_code == 404:
-            # Subreddit not found
-            return 0
-        else:
-            # Other error occurred
-            return 0
-    except requests.exceptions.RequestException:
-        # Handle connection error
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-
-# Example usage
-subreddit_name = "funny"
-subscribers = number_of_subscribers(subreddit_name)
-if subscribers > 0:
-    print(f"The subreddit '{subreddit_name}' has {subscribers} subscribers.")
-else:
-    print(f"'{subreddit_name}' is not a valid subreddit.")
